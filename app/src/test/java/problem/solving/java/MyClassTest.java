060ea878b79d04496bb6b2245fc3605f618e09d7
yusunghyun <ysh1513@naver.com>
2023-08-22 14:30:52 +0900

    🔧 add java prettier

--- app/src/test/java/problem/solving/java/MyClassTest.java
+++ app/src/test/java/problem/solving/java/MyClassTest.java
@@ -1,17 +1,16 @@
 package test.java.problem.solving.java;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import main.java.problem.solving.java.MyClass;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class MyClassTest {
 
-    @Test
-    public void testSubtract() {
-        MyClass myClass = new MyClass();
-        int result = myClass.subtract(10, 3);
-        assertEquals(7, result);
-    }
+  @Test
+  public void testSubtract() {
+    MyClass myClass = new MyClass();
+    int result = myClass.subtract(10, 3);
+    assertEquals(7, result);
+  }
 }
